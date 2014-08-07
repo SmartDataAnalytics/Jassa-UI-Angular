f507f1b346cde3dd05ece29583baa9a2925897da
cstadler <cstadler@informatik.uni-leipzig.de>
2014-08-07 14:09:41 +0200

    Updated the demo to use proper npm/grunt/bower config

--- jassa-ui-angular-openlayers/demo/jassa-map-ol-basic/bower-update.sh
+++ jassa-ui-angular-openlayers/demo/jassa-map-ol-basic/bower-update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# NOTE: Starting with bower v1.3.6, you can now use
+# bower install --force
+# instead of this script http://stackoverflow.com/questions/20226232/bower-force-update-of-local-package
+
 rm -rf app/bower_components
 bower cache clean
 bower install
