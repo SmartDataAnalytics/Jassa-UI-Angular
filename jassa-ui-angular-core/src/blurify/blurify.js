2df84d1a83455e60e389f28b728ac07b34654477
cstadler <cstadler@informatik.uni-leipzig.de>
2014-09-11 01:13:30 +0200

    Added blurify directive

---
+++ jassa-ui-angular-core/src/blurify/blurify.js
@@ -0,0 +1,40 @@
+angular.module('ui.jassa.blurify', [])
+
+/**
+ * Replaces text content with an alternative on blur
+ * blurify="(function(model) { return 'displayValue'; })"
+ *
+ */
+.directive('blurify', [ '$parse', function($parse) {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function($scope, element, attrs, model) {
+            element.on('focus', function () {
+                // Re-render the model on focus
+                model.$render();
+            });
+            element.on('blur', function () {
+                var modelVal = $parse(attrs['ngModel'])($scope);
+                var labelFn = $parse(attrs['blurify'])($scope);
+
+                if(labelFn) {
+                    var val = labelFn(modelVal);
+                    if(val.then) {
+                        val.then(function(label) {
+                            element.val(label);
+                        });
+                    } else {
+                        element.val(val);
+                    }
+                }
+//              $scope.$apply(function() {
+//                  model.$setViewValue(val);
+//              });
+            });
+        }
+    };
+}])
+
+;
+
