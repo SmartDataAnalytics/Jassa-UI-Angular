0f25956bee2de7a1261987a9c58e7372fb09f09b
cstadler <cstadler@informatik.uni-leipzig.de>
2014-09-16 16:35:56 +0200

    fixes

--- jassa-ui-angular-core/src/jassa-media-list/jassa-media-list.js
+++ jassa-ui-angular-core/src/jassa-media-list/jassa-media-list.js
@@ -17,7 +17,8 @@ angular.module('ui.jassa.jassa-media-list', [])
         $scope.offset = ($scope.currentPage - 1) * $scope.limit;
     });
 
-    $scope.$watch('[offset, totalItems, filter, refresh]', $scope.doRefresh, true);
+    $scope.$watch('[filter, limit, offset, refresh]', $scope.doRefresh, true);
+    $scope.$watch('listService', $scope.doRefresh);
 }])
 
 .directive('jassaMediaList', [function() {
@@ -34,6 +35,7 @@ angular.module('ui.jassa.jassa-media-list', [])
             totalItems: '=',
             currentPage: '=',
             items: '=',
+            maxSize: '=',
             refresh: '=' // Extra attribute that is deep watched on changes for triggering refreshs
         },
         controller: 'JassaMediaListCtrl',
