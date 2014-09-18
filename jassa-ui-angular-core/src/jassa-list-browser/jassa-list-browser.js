ed18d7a8babfa76c3794d6c9b52ebc1bd4180a21
cstadler <cstadler@informatik.uni-leipzig.de>
2014-09-18 02:17:55 +0200

    Added jassa-list-browser directive

---
+++ jassa-ui-angular-core/src/jassa-list-browser/jassa-list-browser.js
@@ -0,0 +1,23 @@
+angular.module('ui.jassa.jassa-list-browser', [])
+
+.directive('jassaListBrowser', function() {
+    return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+            listService: '=',
+            filter: '=',
+            limit: '=',
+            offset: '=',
+            totalItems: '=',
+            items: '=',
+            maxSize: '=',
+            langs: '=', // Extra attribute that is deep watched on changes for triggering refreshs
+            availableLangs: '=',
+            context: '=' // Extra data that can be passed in // TODO I would prefer access to the parent scope
+        },
+        templateUrl: 'template/jassa-list-browser/jassa-list-browser.html'
+    };
+})
+
+;
