17eadbc894dfba427497dff9b499f9a54d3ec7b2
cstadler <cstadler@informatik.uni-leipzig.de>
2014-11-29 03:05:54 +0100

    Added example for WKT

--- jassa-ui-angular-edit/demo/rex/demo1/app/utils.js
+++ jassa-ui-angular-edit/demo/rex/demo1/app/utils.js
@@ -130,3 +130,47 @@ var createRexLookupService = function(sparqlService) {
 
     return lookupService;
 };
+
+
+var PointUtils = {
+    lonlatToWkt: function(lonlat) {
+        var result = this.pointToWkt(lonlat.lon, lonlat.lat);
+        return result;
+    },
+
+    xyToWkt: function(xy) {
+        var result = this.pointToWkt(xy.x, xy.y);
+        return result;
+    },
+
+    pointToWkt: function(x, y) {
+        var result = 'POINT (' + x + ' ' + y + ')';
+        return result;
+    },
+
+    wktPointRegex: /\s*POINT\s*\(([^\s]+)\s*([^)]+)\)\s*/i,
+
+    isWktPoint: function(wktStr) {
+        var match = this.wktPointRegex.exec(wktStr);
+        var result = !!match;
+        return result;
+    },
+
+    wktToXy: function(wktStr) {
+        var match = this.wktPointRegex.exec(wktStr);
+        if(!match) {
+            return null;
+        }
+
+        var xStr = match[1];
+        var yStr = match[2];
+
+        var result = {
+            x: parseFloat(xStr),
+            y: parseFloat(yStr)
+        };
+
+        return result;
+    }
+};
+
