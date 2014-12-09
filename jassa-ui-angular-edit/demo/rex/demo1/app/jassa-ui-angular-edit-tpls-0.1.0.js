247738b34622d8d7ee6a7bcd8e53ab4d92370ffe
Clemens Hoffmann <cannelony@gmail.com>
2014-12-09 23:20:25 +0100

    change placeholder for termtype

--- jassa-ui-angular-edit/demo/rex/demo1/app/jassa-ui-angular-edit-tpls-0.1.0.js
+++ jassa-ui-angular-edit/demo/rex/demo1/app/jassa-ui-angular-edit-tpls-0.1.0.js
@@ -229,8 +229,9 @@ angular.module('ui.jassa.rdf-term-input', [])
 
                           // if the datatype is not in hashmap add them
                           if (!matchedDatatype) {
-                            // create new datatype set
+                            //TODO: short uri for displayLabel
                             var prefixMapping = new jassa.rdf.PrefixMappingImpl();
+                            // create new datatype set
                             var newDatatype = {
                               id: scope.state.datatype,
                               displayLabel:  prefixMapping.shortForm(scope.state.datatype)
@@ -1639,7 +1640,7 @@ angular.module("template/rdf-term-input/rdf-term-input.html", []).run(["$templat
     "    <div class=\"input-group-addon\">\n" +
     "        <!--select ng-model=\"state.type\" ng-model-options=\"ngModelOptions\" ng-options=\"item.id as item.displayLabel for item in termTypes\" ng-change=\"ensureValidity()\"></select-->\n" +
     "        <ui-select ng-model=\"termTypes.selected\" ng-disabled=\"disabled\" theme=\"selectize\"  reset-search-input=\"false\" on-select=\"onSelectTermType($item, $model)\" style=\"width: 100px;\" >\n" +
-    "          <ui-select-match placeholder=\"Select or search in the list...\">{{$select.selected.displayLabel}}</ui-select-match>\n" +
+    "          <ui-select-match placeholder=\"Termtype\">{{$select.selected.displayLabel}}</ui-select-match>\n" +
     "          <ui-select-choices repeat=\"item in termTypes | filter: $select.search\">\n" +
     "            <span ng-bind-html=\"item.displayLabel | highlight: $select.search\"></span>\n" +
     "          </ui-select-choices>\n" +
