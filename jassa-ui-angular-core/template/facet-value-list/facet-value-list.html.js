47147caf35190f02eb926b8d81868c85da2f0774
cstadler <cstadler@informatik.uni-leipzig.de>
2015-01-07 15:30:07 +0100

    Another fix

--- jassa-ui-angular-core/template/facet-value-list/facet-value-list.html.js
+++ jassa-ui-angular-core/template/facet-value-list/facet-value-list.html.js
@@ -1,7 +1,6 @@
 angular.module("template/facet-value-list/facet-value-list.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/facet-value-list/facet-value-list.html",
     "<div class=\"frame\">\n" +
-    "<<<<<<< HEAD\n" +
     "    <form ng-submit=\"filterTable(filterText)\">\n" +
     "        <input type=\"text\" ng-model=\"filterText\" />\n" +
     "        <input class=\"btn-primary\" type=\"submit\" value=\"Filter\" />\n" +
@@ -22,29 +21,7 @@ angular.module("template/facet-value-list/facet-value-list.html", []).run(["$tem
     "<!--               </tr> -->\n" +
     "<!--           </table> -->\n" +
     "          <pagination class=\"pagination-small\" total-items=\"pagination.totalItems\" page=\"pagination.currentPage\" max-size=\"pagination.maxSize\" boundary-links=\"true\" rotate=\"false\" num-pages=\"pagination.numPages\" previous-text=\"&lsaquo;\" next-text=\"&rsaquo;\" first-text=\"&laquo;\" last-text=\"&raquo;\"></pagination>\n" +
-    "=======\n" +
-    "	<form ng-submit=\"filterTable(filterText)\">\n" +
-    "	    <input type=\"text\" ng-model=\"filterText\" />\n" +
-    "		<input class=\"btn-primary\" type=\"submit\" value=\"Filter\" />\n" +
-    "	</form>\n" +
-    "\n" +
-    "	<span ng-show=\"loading.data || loading.pageCount\">\n" +
-    "		Loading... \n" +
-    "	    <span ng-show=\"loading.data\">(data)</span>\n" +
-    "	    <span ng-show=\"loading.pageCount\">(page count)</span>\n" +
-    "	</span>\n" +
-    "\n" +
-    "	<table>\n" +
-    "              <tr><th>Value</th><th>Constrained</th></tr>\n" +
-    "<!-- <th>Count</th> -->\n" +
-    "	    <tr ng-repeat=\"item in facetValues\">\n" +
-    "                  <td><span title=\"{{item.node.toString()}}\">{{item.displayLabel}}</span></td>\n" +
-    "<!--                    <td>todo</td> -->\n" +
-    "                  <td><input type=\"checkbox\" ng-model=\"item.tags.isConstrainedEqual\" ng-change=\"toggleConstraint(item)\" /></td>\n" +
-    "              </tr>\n" +
-    "      	</table>\n" +
-    "  		<pagination class=\"pagination-small\" total-items=\"pagination.totalItems\" page=\"pagination.currentPage\" max-size=\"pagination.maxSize\" boundary-links=\"true\" rotate=\"false\" num-pages=\"pagination.numPages\" previous-text=\"&lsaquo;\" next-text=\"&rsaquo;\" first-text=\"&laquo;\" last-text=\"&raquo;\"></pagination>\n" +
-    ">>>>>>> origin\n" +
     "</div>\n" +
+    "\n" +
     "");
 }]);
