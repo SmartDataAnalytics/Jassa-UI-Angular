58d90a3cc5869df42d34c7f08c575edce0a08ce4
cstadler <cstadler@informatik.uni-leipzig.de>
2015-02-05 17:50:05 +0100

    updates to the facet list

--- jassa-ui-angular-core/template/facet-list/facet-list.html.js
+++ jassa-ui-angular-core/template/facet-list/facet-list.html.js
@@ -2,6 +2,7 @@ angular.module("template/facet-list/facet-list.html", []).run(["$templateCache",
   $templateCache.put("template/facet-list/facet-list.html",
     "<div>\n" +
     "\n" +
+    "    <!-- Notification when service is missing -->\n" +
     "    <div ng-if=\"!ls.ctrl.listService\" class=\"alert alert-info\">\n" +
     "        <span class=\"glyphicon glyphicon-exclamation-sign\"></span>\n" +
     "        No service configured for the facet list.\n" +
@@ -10,19 +11,31 @@ angular.module("template/facet-list/facet-list.html", []).run(["$templateCache",
     "    <!-- Breadcrumb -->\n" +
     "    <breadcrumb sparql-service=\"sparqlService\" ng-model=\"breadcrumb\"></breadcrumb>\n" +
     "\n" +
-    "    <!-- Filter text input field -->\n" +
-    "    <div>\n" +
-    "        <form role=\"form\" ng-submit=\"ls.ctrl.filter.concept=filterModel; listFilter.offset=0\" novalidate>\n" +
-    "            <div class=\"input-group\">\n" +
-    "                <input ng-model=\"filterModel\" type=\"text\" class=\"form-control facet-filter\" placeholder=\"Find ...\">\n" +
-    "                <span class=\"input-group-btn facet-filter-submit\">\n" +
-    "                    <button type=\"submit\" class=\"btn btn-default\" type=\"button\"><span class=\"glyphicon glyphicon-search\"></span></button>\n" +
-    "                </span>\n" +
+    "    <form role=\"form\" class=\"form-inline\" ng-submit=\"ls.ctrl.filter.concept=filterModel; listFilter.offset=0\" novalidate>\n" +
+    "        <div class=\"form-group\">\n" +
+    "            <div class=\"col-sm-7\">\n" +
+    "                <div class=\"input-group\">\n" +
+    "                    <input ng-model=\"filterModel\" type=\"text\" class=\"form-control facet-filter\" placeholder=\"Find ...\">\n" +
+    "                    <span ng-if=\"ls.ctrl.filter.concept\" class=\"input-group-btn facet-filter-submit\">\n" +
+    "                        <button class=\"btn btn-default\" type=\"button\" ng-click=\"ls.ctrl.filter.concept=''\"><span class=\"glyphicon glyphicon glyphicon-remove-circle\"></span></button>\n" +
+    "                    </span>\n" +
+    "                    <span class=\"input-group-btn facet-filter-submit\">\n" +
+    "                        <button type=\"submit\" class=\"btn btn-default\" type=\"button\"><span class=\"glyphicon glyphicon-search\"></span></button>\n" +
+    "                    </span>\n" +
+    "                </div>\n" +
     "            </div>\n" +
-    "        </form>\n" +
     "\n" +
-    "        <div ng-show=\"ls.ctrl.filter.concept.length > 0\" style=\"margin: 5px 0 0 10px; color: #aaa;\"><span ng-show=\"ls.loading.data || ls.loading.pageCount\">Filtering by</span><span ng-hide=\"ls.loading.data || ls.loading.pageCount\">Filtered by </span> '{{ls.ctrl.filter.concept}}'</div>\n" +
-    "    </div>\n" +
+    "            <div class=\"col-sm-5\">\n" +
+    "                <div class=\"input-group\" ng-init=\"showOptions=[{value: 10, label: '10'}, {value: 25, label: 'twenty five'}, {value: 50, label: '50'}, {value: 100, label: '100'}]\">\n" +
+    "                    <span class=\"input-group-addon\">Show </span>\n" +
+    "                    <select class=\"form-control\" type=\"text\" ng-model=\"ls.ctrl.filter.limit\"  ng-model-options=\"showOptions\" ng-options=\"option.value as option.label for option in showOptions\"></select>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "\n" +
+    "    </form>\n" +
+    "\n" +
+    "    <div ng-show=\"ls.ctrl.filter.concept.length > 0\" style=\"margin: 5px 0 0 10px; color: #aaa;\"><span ng-show=\"ls.loading.data || ls.loading.pageCount\">Filtering by</span><span ng-hide=\"ls.loading.data || ls.loading.pageCount\">Filtered by </span> '{{ls.ctrl.filter.concept}}'</div>\n" +
     "\n" +
     "    <!-- Navigation buttons -->\n" +
     "    <div style=\"width: 100%\">\n" +
@@ -36,20 +49,17 @@ angular.module("template/facet-list/facet-list.html", []).run(["$templateCache",
     "    </div>\n" +
     "\n" +
     "    <!-- TODO Loading data icon -->\n" +
-    "\n" +
-    "    <div class=\"form-group\">\n" +
-    "        <div class=\"input-group\" ng-init=\"showOptions=[{value: 10, label: '10'}, {value: 25, label: '25'}, {value: 50, label: '50'}, {value: 100, label: '100'}]\">\n" +
-    "            <span class=\"input-group-addon\">Show: </span>\n" +
-    "            <select class=\"form-control\" ng-model=\"ls.ctrl.filter.limit\"  ng-model-options=\"showOptions\" ng-options=\"option.value as option.label for option in showOptions\"></select>\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "\n" +
     "    <!-- Paginator -->\n" +
     "    <div style=\"width: 100%; text-align: center\">\n" +
     "        <span ng-show=\"ls.loading.pageCount\" class=\"glyphicon glyphicon-refresh\"></span>\n" +
     "\n" +
     "        <pagination ng-show=\"ls.state.paging.numPages > 1\" class=\"pagination pagination-sm\" paging-model=\"ls\" paging-style=\"pagingStyle\"></pagination>\n" +
     "    </div>\n" +
+    "\n" +
+    "    <!-- Pagination status -->\n" +
+    "    <span style=\"margin: 5px 0 0 10px; color: #aaa;\">\n" +
+    "    Showing {{ls.state.items.length}} entries in the positions {{(ls.state.paging.currentPage - 1) * ls.state.filter.limit + (ls.state.items.length ? 1 : 0)}} - {{(ls.state.paging.currentPage - 1) * ls.state.filter.limit + ls.state.items.length}} out of {{ls.state.paging.totalItems}} items in total.\n" +
+    "    </span>\n" +
     "\n" +
     "    <ul ng-show=\"!ls.state.items.length\" class=\"list-group facet-list\">\n" +
     "        <li class=\"list-group-item facet-list-item\" style=\"text-align: center\">\n" +
@@ -62,8 +72,6 @@ angular.module("template/facet-list/facet-list.html", []).run(["$templateCache",
     "    <!-- Data list -->\n" +
     "    <ul ng-show=\"!showConstraints && !ls.loading.data\" class=\"list-group facet-list\">\n" +
     "        <li ng-repeat=\"item in ls.state.items\" class=\"list-group-item facet-list-item show-on-hover-parent\" ng-class=\"facetValuePath==null?'facet':'facet-value'\">\n" +
-    "\n" +
-    "<!--                 <template-list style=\"list-style:none; display: inline; padding-left: 0px;\" templates=\"plugins\" data=\"facet\" context=\"pluginContext\"></template-list> -->\n" +
     "\n" +
     "            <div ng-show=\"facetValuePath==null\">\n" +
     "                <button style=\"text-align: left;\" class=\"btn btn-default btn-label facet-list-item-btn\" type=\"button\" ng-click=\"breadcrumb.property = item.property.getUri()\">\n" +
