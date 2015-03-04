17cd21cf8e1ca8bf890f386a860988cee37702ba
cstadler <cstadler@informatik.uni-leipzig.de>
2015-03-04 23:58:11 +0100

    maybe pristine is now (fully?) working for rdf-term-input

--- jassa-ui-angular-edit/src/rex/00-rex-header.js
+++ jassa-ui-angular-edit/src/rex/00-rex-header.js
@@ -122,7 +122,8 @@ var createCompileComponent = function($rexComponent$, $component$, $parse, oneWa
             }, function(after, before) {
                 if(after && after != before) {
                     if(modelSetter) {
-                        var value = getValueAt(scope.rexContext.json, newCoordinate);
+                        var coordinate = createCoordinate(scope, $component$);
+                        var value = getValueAt(scope.rexContext.json, coordinate);
                         modelSetter(scope, value);
                     }
                 }
