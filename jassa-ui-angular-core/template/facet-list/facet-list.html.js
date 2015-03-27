3d0f024830342f1af639b96bb306023d68699049
cstadler <cstadler@informatik.uni-leipzig.de>
2015-03-27 15:53:12 +0100

    update

--- jassa-ui-angular-core/template/facet-list/facet-list.html.js
+++ jassa-ui-angular-core/template/facet-list/facet-list.html.js
@@ -8,6 +8,8 @@ angular.module("template/facet-list/facet-list.html", []).run(["$templateCache",
     "        No service configured (yet).\n" +
     "    </div>\n" +
     "\n" +
+    "<!-- Loading - data: {{ls.loading.data}} - pages: {{ls.loading.pageCount}} -->\n" +
+    "\n" +
     "    <!-- Breadcrumb -->\n" +
     "<!--     <breadcrumb sparql-service=\"sparqlService\" ng-model=\"breadcrumb\"></breadcrumb> -->\n" +
     "    <breadcrumb ng-show=\"!showConstraints\" lookup-service=\"lookupServiceNodeLabels\" ng-model=\"breadcrumb\"></breadcrumb>\n" +
@@ -38,7 +40,9 @@ angular.module("template/facet-list/facet-list.html", []).run(["$templateCache",
     "\n" +
     "    </form>\n" +
     "\n" +
-    "    <div ng-show=\"ls.ctrl.filter.concept.length > 0\" style=\"margin: 5px 0 0 10px; color: #aaa;\"><span ng-show=\"ls.loading.data || ls.loading.pageCount\">Filtering by</span><span ng-hide=\"ls.loading.data || ls.loading.pageCount\">Filtered by </span> '{{ls.ctrl.filter.concept}}'</div>\n" +
+    "    <div ng-show=\"ls.ctrl.filter.concept.length > 0\" style=\"margin: 5px 0 0 10px; color: #aaa;\">\n" +
+    "        <span ng-show=\"ls.loading.data || ls.loading.pageCount\">Filtering by</span><span ng-hide=\"ls.loading.data || ls.loading.pageCount\">Filtered by </span> '{{ls.ctrl.filter.concept}}'\n" +
+    "    </div>\n" +
     "\n" +
     "    <!-- Navigation buttons -->\n" +
     "    <div style=\"width: 100%\">\n" +
@@ -56,11 +60,11 @@ angular.module("template/facet-list/facet-list.html", []).run(["$templateCache",
     "    <div style=\"width: 100%; text-align: center\">\n" +
     "        <span ng-show=\"ls.loading.pageCount\" class=\"glyphicon glyphicon-refresh\"></span>\n" +
     "\n" +
-    "        <pagination ng-show=\"ls.state.paging.numPages > 1\" class=\"pagination pagination-sm\" paging-model=\"ls\" paging-style=\"pagingStyle\"></pagination>\n" +
+    "        <pagination ng-show=\"!ls.loading.pageCount && ls.state.paging.numPages > 1\" class=\"pagination pagination-sm\" paging-model=\"ls\" paging-style=\"pagingStyle\"></pagination>\n" +
     "    </div>\n" +
     "\n" +
     "    <!-- Pagination status -->\n" +
-    "    <span style=\"margin: 5px 0 0 10px; color: #aaa;\">\n" +
+    "    <span ng-show=\"!ls.loading.pageCount\" style=\"margin: 5px 0 0 10px; color: #aaa;\">\n" +
     "    Showing {{ls.state.items.length}} entries in the positions {{(ls.state.paging.currentPage - 1) * ls.state.filter.limit + (ls.state.items.length ? 1 : 0)}} - {{(ls.state.paging.currentPage - 1) * ls.state.filter.limit + ls.state.items.length}} out of {{ls.state.paging.totalItems}} items in total.\n" +
     "    </span>\n" +
     "\n" +
@@ -75,13 +79,18 @@ angular.module("template/facet-list/facet-list.html", []).run(["$templateCache",
     "    <!-- Data list -->\n" +
     "    <ul ng-show=\"!ls.loading.data\" class=\"list-group facet-list\">\n" +
     "        <li ng-repeat=\"item in ls.state.items\" class=\"list-group-item facet-list-item visible-on-hover-parent\" ng-class=\"facetValuePath==null?'facet':'facet-value'\">\n" +
-    "\n" +
     "            <div ng-include=\"mode.itemTemplate\"></div>\n" +
-    "\n" +
     "        </li>\n" +
     "    </ul>\n" +
     "\n" +
-    "    <div style=\"width: 100%\">\n" +
+    "    <ul ng-show=\"ls.loading.data\" class=\"list-group facet-list\">\n" +
+    "        <li class=\"list-group-item facet-list-item\" style=\"text-align: center\">\n" +
+    "            <span ng-show=\"ls.loading.pageCount\" class=\"glyphicon glyphicon-refresh\"></span>\n" +
+    "        </li>\n" +
+    "    </ul>\n" +
+    "\n" +
+    "\n" +
+    "    <div ng-show=\"showConstraints\" style=\"width: 100%\">\n" +
     "        <button style=\"text-align: left; width: 100%\" ng-class=\"constraintManager.getConstraints().length ? '' : 'disabled'\" class=\"btn btn-warning\" type=\"button\" ng-click=\"constraintManager.clear()\">\n" +
     "            <span class=\"glyphicon glyphicon-remove-circle\"></span> Clear Filters\n" +
     "        </button>\n" +
