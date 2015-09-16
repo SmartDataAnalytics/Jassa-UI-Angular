d51754901b6db26cb70ca6c7af9e285301da3b33
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2015-09-17 00:57:39 +0200

    added clearing the target dir

--- jassa-ui-angular-core/build.sh
+++ jassa-ui-angular-core/build.sh
@@ -13,8 +13,8 @@ grunt
 git clone git@github.com:GeoKnow/Jassa-UI-Angular-Bower.git "$targetFolder" || true
 ( cd "$targetFolder" && git pull )
 
-#rm -rf "$targetFolder"
-#mkdir -p "$targetFolder"
+rm -rf "$targetFolder"
+mkdir -p "$targetFolder"
 
 cp bower.json "$targetFolder"
 cp css/jassa-ui-angular.css "$targetFolder"
