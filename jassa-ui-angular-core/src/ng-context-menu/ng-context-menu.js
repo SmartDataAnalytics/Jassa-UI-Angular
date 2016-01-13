7f307149008e0ac50b6c7376b88bbe26695aeee3
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2016-01-13 18:31:08 +0100

    fixed module declaration

--- jassa-ui-angular-core/src/ng-context-menu/ng-context-menu.js
+++ jassa-ui-angular-core/src/ng-context-menu/ng-context-menu.js
@@ -123,7 +123,7 @@ var ContextMenuUtils = {
 
 
 
-angular.module('ui.jassa.ng-context-menu')
+angular.module('ui.jassa.ng-context-menu', [])
 
 .factory('ngContextMenuFactory', ['$compile', '$templateCache', '$http', function($compile, $templateCache, $http) {
     var createContentPromise = function(options) {
